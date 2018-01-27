4dbe2e65154f2de60da8e4750a73855d70ceb392
danielxu <xwd.forever@gmail.com>
2018-01-27 00:50:36 -0500

    Refactoring to match the issue

--- lib/course_schedule.ex
+++ lib/course_schedule.ex
@@ -1,8 +1,8 @@
 defmodule CourseSchedule do
   def run(n, deps) do
-    # %{0 => [1], 1 => [2, 0]}
-    s = Enum.reduce(deps, %{}, fn([x | y], acc) ->
-      Map.update(acc, x, List.wrap(y), &(&1 ++ y))
+    # %{0 => 1, 1 => 2}
+    s = Enum.reduce(deps, %{}, fn([x, y], acc) ->
+      Map.put(acc, x, y)
     end)
 
     Enum.reduce_while(s, false, fn({n, _v}, acc) ->
@@ -13,15 +13,12 @@ defmodule CourseSchedule do
   @doc """
   return true/false
   true: has cycle
-
-  # %{0 => [1], 1 => [2, 0]}
   """
   def check(s, current, target) do
-    Map.get(s, current, [])
-    |> Enum.reduce_while(false, fn(n, _acc) ->
-      if n == target,
-        do: {:halt, true},
-        else: {:cont, check(s, n, target)}
-    end)
+    case Map.get(s, current) do
+      nil -> false
+      ^target -> true
+      n -> check(s, n, target)
+    end
   end
 end
