a5eed515149ee0f249c6da3f39731b2eff0ec9a0
danielxu <xwd.forever@gmail.com>
2018-03-07 18:58:51 -0500

    Improve comment

--- lib/DP/longest_increasing_seq.ex
+++ lib/DP/longest_increasing_seq.ex
@@ -7,10 +7,16 @@ defmodule LongestIncreasingSeq do
   @doc """
   [5, 3, 4, 8, 6, 7]
   sub problem: d(n-1) include n,
-  5 -> 1
-  5, 3 -> 1
-  5, 3, 4, if a[x] < a[i], j = 0 else, j = 1, max(d[0] + a, d[1] +a)
-  5, 3, 4, 8
+  5 -> (5, 1)
+  5, 3 -> %{(5, 1), (3, 1)}
+  5, 3, 4 -> %{(5, 1), (3, 1), (4, 2)}
+  5, 3, 4, 8 -> %{(5, 1), (3, 1), (4, 2), (8, 3)}
+  5, 3, 5, 8, 6 -> %{(5, 1), (3, 1), (4, 2), (8, 3), (6, 3)}
+
+  how do we calculate 6?
+  we have a map in previous iteration %{(5, 1), (3, 1), (4, 2), (8, 3)}
+  we found that 5, 3, 4 less than 6
+  and the max is (4, 2) => 2 + 1 => (6, 3)
   """
   def process([], _current, max), do: max
 
