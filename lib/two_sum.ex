f8d3366aa2b020f32e3d2a9277a31b0c580f7d0d
danielxu <xwd.forever@gmail.com>
2018-03-12 16:39:15 -0400

    Two sum

--- lib/two_sum.ex
+++ lib/two_sum.ex
@@ -1,18 +1,40 @@
 defmodule TwoSum do
+  # def run(nums, target) do
+  #   Enum.with_index(nums)
+  #   |> process(target, false, [])
+  # end
+
+  # def process([], target, true, acc), do: acc
+
+  # def process([{h_v, h_i} | t], target, stop, acc) do
+  #   value = Enum.find(t, fn {v, _v_i} -> v + h_v == target end)
+
+  #   if value == nil,
+  #     do: process(t, target, stop, acc),
+  #     else: process(t, target, true, [h_i, elem(value, 1)])
+  # end
+
+  # def process(_, _, _, _), do: []
+
+  @doc """
+  we can use two pointer, small and big
+  if v[small] + v[big] > target,
+  -> small++
+  if v[small] + v[big] < target
+  -> big--
+  """
+  def run([], _target), do: []
   def run(nums, target) do
-    Enum.with_index(nums)
-    |> process(target, false, [])
+    process(nums, target, 0, length(nums) - 1)
   end
 
-  def process([], target, true, acc), do: acc
-
-  def process([{h_v, h_i} | t], target, stop, acc) do
-    value = Enum.find(t, fn {v, _v_i} -> v + h_v == target end)
-
-    if value == nil,
-      do: process(t, target, stop, acc),
-      else: process(t, target, true, [h_i, elem(value, 1)])
-  end
-
-  def process(_, _, _, _), do: []
+  def process(_nums, _target, small, big) when small >= big, do: []
+  def process(nums, target, small, big) do
+    n = Enum.at(nums, small) + Enum.at(nums, big)
+    case n do
+      ^target -> [small, big]
+      v when v > target -> process(nums, target, small, big-1)
+      _ -> process(nums, target, small+1, big)
+    end
+ end
 end
--- test/leetcode_test.exs
+++ test/leetcode_test.exs
@@ -2,7 +2,8 @@ defmodule LeetcodeTest do
   use ExUnit.Case
 
   test "two sum" do
-    assert TwoSum.run([3, 2, 4], 6) == [1, 2]
+    assert TwoSum.run([1, 2, 4], 6) == [1, 2]
+    assert TwoSum.run([1, 2, 4], 10) == []
     assert TwoSum.run([2, 7, 11, 15], 9) == [0, 1]
   end
 
